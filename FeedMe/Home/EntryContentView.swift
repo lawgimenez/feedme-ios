62e796610b64e449526a98212beb4ee087954a5d
Lawrence Gimenez <lawgimenez@icloud.com>
2022-12-07 20:28:57 +0800

    Can now read content by converting html to string

--- FeedMe/Home/EntryContentView.swift
+++ FeedMe/Home/EntryContentView.swift
@@ -14,7 +14,38 @@ struct EntryContentView: View {
     var body: some View {
         let _ = print(entry.extractedURL)
         if let content = entry.content {
-            Text(content)
+            ScrollView {
+                Text(content.html2String)
+            }
         }
     }
 }
+
+extension Data {
+
+    var html2AttributedString: NSAttributedString? {
+        do {
+            return try NSAttributedString(data: self, options: [
+                .documentType: NSAttributedString.DocumentType.html,
+                .characterEncoding: String.Encoding.utf8.rawValue
+            ], documentAttributes: nil)
+        } catch {
+            return  nil
+        }
+    }
+    var html2String: String {
+        return html2AttributedString?.string ?? ""
+    }
+}
+
+
+extension String {
+
+    var html2AttributedString: NSAttributedString? {
+        return Data(utf8).html2AttributedString
+    }
+    var html2String: String {
+        return html2AttributedString?.string ?? ""
+    }
+}
+
