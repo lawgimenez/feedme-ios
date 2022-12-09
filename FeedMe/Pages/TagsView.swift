7885720d7f72c52e7ba334d9cc295ab85d546d48
Lawrence Gimenez <lawgimenez@icloud.com>
2022-12-09 17:01:43 +0800

    Initial tagging page feature implementation

--- FeedMe/Pages/TagsView.swift
+++ FeedMe/Pages/TagsView.swift
@@ -9,11 +9,38 @@ import SwiftUI
 
 struct TagsView: View {
     
+    @State private var arrayTaggings = [Tag]()
+    
     var body: some View {
         VStack {
             Text("Hello, World!")
         }
+        .task {
+            await getTaggings()
+        }
         .navigationTitle("Tags")
+    }
+    
+    private func getTaggings() async {
+        if let url = URL(string: Urls.Api.taggings) {
+            let userValue = String(format: "%@:%@", UserDefaults.standard.string(forKey: Keys.Auth.email)!, UserDefaults.standard.string(forKey: Keys.Auth.password)!).data(using: .utf8)?.base64EncodedString()
+            let session = URLSession.shared
+            var request = URLRequest(url: url)
+            request.httpMethod = "GET"
+            request.setValue("Basic \(userValue!)", forHTTPHeaderField: "Authorization")
+            let task = session.dataTask(with: request) { data, _, _ in
+                if let data = data {
+                    do {
+                        let arrayTaggings = try JSONDecoder().decode([Tag].self, from: data)
+                        print("Array tags = \(arrayTaggings)")
+                        self.arrayTaggings = arrayTaggings
+                    } catch {
+                        print("HomeView.error = \(error)")
+                    }
+                }
+            }
+            task.resume()
+        }
     }
 }
 
--- FeedMe/Pages/UnreadView.swift
+++ FeedMe/Pages/UnreadView.swift
@@ -9,7 +9,6 @@ import SwiftUI
 
 struct UnreadView: View {
     
-    @State private var arrayTaggings = [Tag]()
     @State private var arrayUnreadEntries = [Entry]()
     
     var body: some View {
@@ -43,28 +42,6 @@ struct UnreadView: View {
                     do {
                         let arrayUnreadEntries = try JSONDecoder().decode([Entry].self, from: data)
                         self.arrayUnreadEntries = arrayUnreadEntries
-                    } catch {
-                        print("HomeView.error = \(error)")
-                    }
-                }
-            }
-            task.resume()
-        }
-    }
-    
-    private func getTaggings() {
-        if let url = URL(string: Urls.Api.taggings) {
-            let userValue = String(format: "%@:%@", UserDefaults.standard.string(forKey: Keys.Auth.email)!, UserDefaults.standard.string(forKey: Keys.Auth.password)!).data(using: .utf8)?.base64EncodedString()
-            let session = URLSession.shared
-            var request = URLRequest(url: url)
-            request.httpMethod = "GET"
-            request.setValue("Basic \(userValue!)", forHTTPHeaderField: "Authorization")
-            let task = session.dataTask(with: request) { data, _, _ in
-                if let data = data {
-                    do {
-                        let arrayTaggings = try JSONDecoder().decode([Tag].self, from: data)
-                        print("Array tags = \(arrayTaggings)")
-                        self.arrayTaggings = arrayTaggings
                     } catch {
                         print("HomeView.error = \(error)")
                     }
