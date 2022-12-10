2ec9278f1bdd546a836de87bbb14790ab0277515
Lawrence Gimenez <lawgimenez@icloud.com>
2022-12-10 12:08:56 +0800

    Can now display feeds count in tags

--- FeedMe/Pages/TagsView.swift
+++ FeedMe/Pages/TagsView.swift
@@ -11,11 +11,17 @@ struct TagsView: View {
     
     @State private var arrayTags = [Tag]()
     @State private var arrayTaggings = [Tag]()
+    @State private var dictTags: [String: Int] = [:]
     
     var body: some View {
         VStack {
             List(arrayTags) { tag in
-                Text(tag.name)
+                HStack {
+                    Text(tag.name)
+                    if let count = dictTags[tag.name] {
+                        Text(String(count))
+                    }
+                }
             }
         }
         .task {
@@ -36,7 +42,10 @@ struct TagsView: View {
                 if let data = data {
                     do {
                         self.arrayTags = try JSONDecoder().decode([Tag].self, from: data)
-                        print("Array tags = \(arrayTags)")
+                        for tag in arrayTags {
+                            dictTags[tag.name] = 0
+                        }
+                        print("Array dicts = \(dictTags)")
                     } catch {
                         print("TagsView.error = \(error)")
                     }
@@ -57,6 +66,10 @@ struct TagsView: View {
                 if let data = data {
                     do {
                         self.arrayTaggings = try JSONDecoder().decode([Tag].self, from: data)
+                        for tagging in arrayTaggings {
+                            let currentCount = dictTags[tagging.name]
+                            dictTags[tagging.name] = currentCount! + 1
+                        }
                         print("Array taggings = \(arrayTaggings)")
                     } catch {
                         print("TagsView.error = \(error)")
