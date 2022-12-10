889c6c5ff6214e106962f12015129c273b642d97
Lawrence Gimenez <lawgimenez@icloud.com>
2022-12-10 11:40:41 +0800

    Updated Tag model and added get taggins implementation

--- FeedMe/Pages/TagsView.swift
+++ FeedMe/Pages/TagsView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct TagsView: View {
     
     @State private var arrayTags = [Tag]()
+    @State private var arrayTaggings = [Tag]()
     
     var body: some View {
         VStack {
@@ -19,6 +20,7 @@ struct TagsView: View {
         }
         .task {
             await getTags()
+            await getTaggings()
         }
         .navigationTitle("Tags")
     }
@@ -35,6 +37,27 @@ struct TagsView: View {
                     do {
                         self.arrayTags = try JSONDecoder().decode([Tag].self, from: data)
                         print("Array tags = \(arrayTags)")
+                    } catch {
+                        print("TagsView.error = \(error)")
+                    }
+                }
+            }
+            task.resume()
+        }
+    }
+    
+    private func getTaggings() async {
+        if let url = URL(string: Urls.Api.taggings) {
+            let userValue = String(format: "%@:%@", UserDefaults.standard.string(forKey: Keys.Auth.email)!, UserDefaults.standard.string(forKey: Keys.Auth.password)!).data(using: .utf8)?.base64EncodedString()
+            let session = URLSession.shared
+            var request = URLRequest(url: url)
+            request.httpMethod = "GET"
+            request.setValue("Basic \(userValue!)", forHTTPHeaderField: "Authorization")
+            let task = session.dataTask(with: request) { data, _, _ in
+                if let data = data {
+                    do {
+                        self.arrayTaggings = try JSONDecoder().decode([Tag].self, from: data)
+                        print("Array taggings = \(arrayTaggings)")
                     } catch {
                         print("TagsView.error = \(error)")
                     }
